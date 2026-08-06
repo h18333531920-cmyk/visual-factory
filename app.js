1fdaff490fa096fdf4fe9d8ec5eef1530973a746
王鑫泽 <wangxinze@LB-wangxinze.local>
2026-08-06 17:14:27 +0800

    chore: 部署最新版本 - 组合功能 + 替换bug修复

--- app.js
+++ app.js
@@ -109,7 +109,7 @@
 
   const config = window.VF_CONFIG || {};
   const LIBRARY_BUCKET = 'vf-library';
-  const TOOL_UI_VERSION = '20260806-template-not-components-v178';
+  const TOOL_UI_VERSION = '20260806-atomic-asset-replace-v179';
   const LIBRARY_SOURCE_PAGE_SIZE = 500;
   const LIBRARY_SOURCE_MAX_ROWS = 5000;
   const LIBRARY_RENDER_STEP = 80;
